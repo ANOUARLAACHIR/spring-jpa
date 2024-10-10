f6ccc7a12c3c9faf21a8fc570a624afa2a180e25
anouar <become.engineer88@gmail.com>
2024-10-10 23:38:52 +0100

    add no args and all args constructor with lombok annotations

--- spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
+++ spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
@@ -1,10 +1,14 @@
 package com.codewa.spring_jpa.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
     private Long id;
     private String name;
