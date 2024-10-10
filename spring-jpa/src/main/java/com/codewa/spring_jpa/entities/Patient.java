8342a825977953f4f47ea82a54ccba1eb6e4ad0b
anouar <become.engineer88@gmail.com>
2024-10-10 23:48:21 +0100

    declaring class as entity and defining id and strategy

--- spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
+++ spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
@@ -1,5 +1,9 @@
 package com.codewa.spring_jpa.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Date birth;
