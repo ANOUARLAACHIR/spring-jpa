842b719d8743c4c788f5767fb7ae388c0571eb83
anouar <become.engineer88@gmail.com>
2024-10-11 23:29:23 +0100

    inserting 3 records into db using save()

--- spring-jpa/src/main/java/com/codewa/spring_jpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/codewa/spring_jpa/SpringJpaApplication.java
@@ -1,12 +1,27 @@
 package com.codewa.spring_jpa;
 
+import com.codewa.spring_jpa.entities.Patient;
+import com.codewa.spring_jpa.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.List;
+
 @SpringBootApplication
-public class SpringJpaApplication {
+public class SpringJpaApplication implements CommandLineRunner {
+	@Autowired
+	private PatientRepository patientRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null, "Anouar", new Date(), false, 50));
+		patientRepository.save(new Patient(null, "Fatima", new Date(), true, 30));
+		patientRepository.save(new Patient(null, "Khalil", new Date(), false, 80));
+	}
 }
--- spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
+++ spring-jpa/src/main/java/com/codewa/spring_jpa/entities/Patient.java
@@ -1,9 +1,6 @@
 package com.codewa.spring_jpa.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,9 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 50)
     private String name;
+    @Temporal(TemporalType.DATE)
     private Date birth;
     private boolean isSick;
     private int score;
